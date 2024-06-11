d60e0a30fc216ff8fd57fe4ba83089e23564adaa
kimdust <mji1223@naver.com>
2024-06-11 18:00:37 +0900

    main

---
+++ assets/js/comm.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".main_slide", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".main_best_slide", {
+  slidesPerView: 5.5,
+  spaceBetween: 30,
+  freeMode: true,
+  centeredSlides: false,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".main_new_slide", {
+  slidesPerView: 5.5,
+  spaceBetween: 30,
+  freeMode: true,
+  centeredSlides: false,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
